1e2b7cbd17d193a1624a3e1d7338831d973d15c0
knyznar <katarzyna.nyznar@gmail.com>
2021-11-06 09:48:43 +0100

    init

---
+++ prediction.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import keras
+import numpy
+
+print ("TensorFlow version: " + tf.__version__)
+print ("Keras version: " + keras.__version__)
+print ("Numpy version: " + numpy.__version__)
