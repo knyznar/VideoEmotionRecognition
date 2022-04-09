7e6ced9b2fab4eb36f40352d7a8de45c3d413329
knyznar <katarzyna.nyznar@gmail.com>
2022-04-09 17:07:26 +0200

    update

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import cv2
 import sys
+import tensorflow as tf
+import keras
+from keras.models import load_model
+import numpy as np
+import os
 
-# cascPath = sys.argv[1]
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+# Import model
+model = load_model('model.h5')
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 video_capture = cv2.VideoCapture(0)
+
+emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
 
 while True:
     # Capture frame-by-frame
@@ -17,15 +32,23 @@ while True:
         scaleFactor=1.1,
         minNeighbors=5,
         minSize=(30, 30),
-        # flags=cv2.cv.CV_HAAR_SCALE_IMAGE
     )
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        crop_frame = frame[y:y + h, x:x + w]
+        gray_frame = cv2.cvtColor(crop_frame, cv2.COLOR_BGR2GRAY)
+        # break
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
+        gray_frame_resized = np.resize(gray_frame, (1, 48, 48))  # cos nie tak z reshapem albo nwm, nie umie w Happy
+        gray_frame_resized = np.expand_dims(gray_frame_resized, -1)
+
+        Y_predicted = model.predict(gray_frame_resized)
+        predicted_emotion = emotions[np.argmax(Y_predicted[0])]
+
+        cv2.putText(frame, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- prediction.py
+++ prediction.py
@@ -1,7 +1,48 @@
 import tensorflow as tf
 import keras
-import numpy
+from keras.models import load_model
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-print ("TensorFlow version: " + tf.__version__)
-print ("Keras version: " + keras.__version__)
-print ("Numpy version: " + numpy.__version__)
+print("TensorFlow version: " + tf.__version__)
+print("Keras version: " + keras.__version__)
+print("Numpy version: " + np.__version__)
+
+
+def translate_emotion_value(value): #function used for visualization purposes only
+  if(value==0):
+    return 'Angry'
+  elif(value==1):
+    return 'Disgust'
+  elif(value==2):
+    return 'Fear'
+  elif(value==3):
+    return 'Happy'
+  elif(value==4):
+    return 'Sad'
+  elif(value==5):
+    return 'Surprise'
+  elif(value==6):
+    return 'Neutral'
+
+
+model = load_model('model.h5')
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+image = Image.open('0.jpg').resize((48, 48), Image.ANTIALIAS)
+data = [np.array(image)]
+image_reshaped = np.resize(image, (1, 48, 48))
+image_reshaped = np.expand_dims(image_reshaped, -1)
+print(image_reshaped.shape)
+print(type(image_reshaped))
+
+Y_predicted = model.predict(image_reshaped)
+print(translate_emotion_value(np.argmax(Y_predicted)))
+print(Y_predicted)
+
+
