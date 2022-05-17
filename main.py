881b91d3c5acb89490a8732ba7d717f5dc19e7dc
knyznar <katarzyna.nyznar@gmail.com>
2022-05-17 21:29:22 +0200

    update

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ from keras.models import load_model
 import numpy as np
 import os
 
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 # Import model
-model = load_model('model.h5')
+model = load_model('model_keras_tuner.h5')
 model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
@@ -17,42 +18,47 @@ model.compile(loss='categorical_crossentropy',
 
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-video_capture = cv2.VideoCapture(0)
+videoCapture = cv2.VideoCapture(0)
 
 emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
+frameCounter = 0
 
 while True:
+    frameCounter += 1
+    if frameCounter % 3 != 0:
+        continue
+
     # Capture frame-by-frame
-    ret, frame = video_capture.read()
+    ret, frame = videoCapture.read()
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
+    facesList = faceCascade.detectMultiScale(
+        grayFrame,
+        scaleFactor=1.2,
         minNeighbors=5,
         minSize=(30, 30),
     )
 
     # Draw a rectangle around the faces
-    for (x, y, w, h) in faces:
+    for (x, y, w, h) in facesList:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        crop_frame = frame[y:y + h, x:x + w]
-        gray_frame = cv2.cvtColor(crop_frame, cv2.COLOR_BGR2GRAY)
-        # break
+        cropFrame = grayFrame[y:y + h, x:x + w]
 
-        gray_frame_resized = np.resize(gray_frame, (1, 48, 48))  # cos nie tak z reshapem albo nwm, nie umie w Happy
-        gray_frame_resized = np.expand_dims(gray_frame_resized, -1)
 
-        Y_predicted = model.predict(gray_frame_resized)
+        cropFrameResized = np.resize(cropFrame, (1, 48, 48))
+        cropFrameResized = np.expand_dims(cropFrameResized, -1)
+
+        Y_predicted = model.predict(cropFrameResized)
         predicted_emotion = emotions[np.argmax(Y_predicted[0])]
 
-        cv2.putText(frame, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-        cv2.imshow('Video', frame)
+        cv2.putText(frame, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 5)
+        cv2.putText(frame, predicted_emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.imshow('Video', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 # When everything is done, release the capture
-video_capture.release()
+videoCapture.release()
 cv2.destroyAllWindows()
